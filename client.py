b4146660fdc5c699743467bd5f6ae455d71bc392
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2024-12-10 12:22:22 -0800

    gracefully handle socket disconnect and closing

--- client.py
+++ client.py
@@ -1,38 +1,50 @@
 import socket
 import threading
+import sys
 
-def receive(sock):
-    while True:
-        try:    
-            data = sock.recv(1024).decode()
-            print(data)
-        except KeyboardInterrupt:
-            return
+def receive(sock, active):
+    sock.settimeout(1.0)
+    try:
+        while active.is_set():
+            try:
+                data = sock.recv(1024).decode()
+                if data:
+                    print(data)
+            except socket.timeout:
+                continue  
+    except Exception as e:
+        print(e)
 
-def send(sock, user_name):
-    while True:
-        try:
+def send(sock, user_name, active):
+    try:
+        while active.is_set():
             msg = input()
             sock.sendall(f"{user_name}: {msg}".encode())
-        except KeyboardInterrupt:
-            return
+    except Exception as e:
+        print(e)
 
-def client():
-    try:
-        print("Please Enter a User Name")
-        user_name = input()
-        c_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        c_sock.connect(("", 50007))
+try:
+    print("Please Enter a User Name")
+    user_name = input()
+    c_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c_sock.connect(("", 50007))
     
-        receiving = threading.Thread(target=receive, args=(c_sock,))
-        sending = threading.Thread(target=send, args=(c_sock, user_name))
+    active = threading.Event()
+    active.set()
+    receiving = threading.Thread(target=receive, args=(c_sock, active))
+    sending = threading.Thread(target=send, args=(c_sock, user_name, active))
     
-        receiving.start()
-        sending.start()
-    
-        receiving.join()
-        sending.join()
-    except KeyboardInterrupt:
-        c_sock.close()
+    receiving.daemon = True
+    sending.daemon = True
+    receiving.start()
+    sending.start()
+    receiving.join()
+    sending.join()
 
-client()
+
+except KeyboardInterrupt:
+    c_sock.sendall(b'')
+    active.clear()
+finally:
+    c_sock.close()
+
